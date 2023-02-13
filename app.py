d81f77cb7e092d60e360b5e5cabf4cf2d85ef1d0
faris-octa <m.faris.octa@gmail.com>
2023-02-13 07:43:46 +0700

    initiate

---
+++ app.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+})
+
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+
+st.set_page_config(page_title="Faris' Webpage", page_icon=":tada:", layout="wide")
+
+# ----HEADER SECTION ----
+st.subheader("Hi, I am Faris")
+st.title("A really bored guy......")
+st.write("Hoaaahmmmm")
+
+st.write("Here's our first attempt at using data to create a table:")
+st.write(df)
+
+st.dataframe(dataframe.style.highlight_max(axis=0))
+
+st.table(dataframe)
+
+
+# ---- chart ----
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+
+# ---- map ----
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
